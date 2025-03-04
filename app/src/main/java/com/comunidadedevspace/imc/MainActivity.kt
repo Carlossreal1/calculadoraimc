b7532db4491aef4644928044d3a7445d4ce7120a
Redux Gamer <joseccrodrigues@hotmail.com>
2025-03-04 21:32:53 +0100

    Snack Preencha todos os campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -17,14 +18,26 @@ class MainActivity : AppCompatActivity() {
 
         btnCalcular.setOnClickListener {
             val pesoStr: String = edtPeso.text.toString()
-            val altura: String = edtAltura.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
 
-            if (pesoStr.isNotEmpty())
-            val alturaQ2 = altura * altura
-            val resultado = pesoStr / alturaQ2
-            println("José Carlos" + resultado )
+            if (pesoStr == "" || alturaStr == "") {
+
+                Snackbar.make(
+                   edtPeso,
+                    "Preencha todos os campos",
+                Snackbar.LENGTH_LONG
+                )
+                    .show()
+            } else {
+                val peso = edtPeso.text.toString().toFloat()
+                val altura = edtAltura.text.toString().toFloat()
+
+
+                val alturaQ2 = altura * altura
+                val resultado = peso / alturaQ2
+                println("José Carlos" + resultado)
+            }
+
         }
-
-
     }
 }
