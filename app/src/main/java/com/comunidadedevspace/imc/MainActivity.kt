91e0c277120fea64a56ccd67051faa8146a40ae6
Redux Gamer <joseccrodrigues@hotmail.com>
2025-03-12 23:08:32 +0100

    Resultado da proxima tela + calculo + cor Estilos+ pagina final

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : AppCompatActivity() {
                 intent.putExtra(KEY_RESULT_IMC , resultado)
                 startActivity(intent)
 
-                println("José Carlos" + resultado)
+                println("José Carlos " + resultado)
             }
 
         }
